346ba0f9c53d38381b41003c9bb75378e6e623cf
Documenter.jl <documenter@juliadocs.github.io>
2021-12-12 10:58:33 +0000

    build based on a0b3f46

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyPkg","category":"page"},{"location":"#jldev_poetry","page":"Home","title":"jldev_poetry","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for jldev_poetry.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyPkg]","category":"page"}]
+}
