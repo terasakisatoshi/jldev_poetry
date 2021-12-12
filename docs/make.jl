bd8a3262648e1e876b519f74af934ee66533cac1
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2021-12-12 19:52:47 +0900

    fix link

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(MyPkg, :DocTestSetup, :(using MyPkg); recursive=true)
 makedocs(;
     modules=[MyPkg],
     authors="SatoshiTerasaki <terasakisatoshi.math@gmail.com> and contributors",
-    repo="https://github.com/terasakisatoshi/jldev_poetry.jl/blob/{commit}{path}#{line}",
-    sitename="jldev_poetry.jl",
+    repo="https://github.com/terasakisatoshi/jldev_poetry/blob/{commit}{path}#{line}",
+    sitename="jldev_poetry",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://terasakisatoshi.github.io/jldev_poetry.jl",
+        canonical="https://terasakisatoshi.github.io/jldev_poetry",
         assets=String[],
     ),
     pages=[
@@ -19,6 +19,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/terasakisatoshi/jldev_poetry.jl",
+    repo="github.com/terasakisatoshi/jldev_poetry",
     devbranch="main",
 )
